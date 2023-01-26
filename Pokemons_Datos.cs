d05b1a5bd53118accf0b006e35b34a8208e14d57
hapm97 <hapm.1997@gmail.com>
2023-01-26 12:54:18 -0300

    Primer subida de Conexion a DB

---
+++ Pokemons_Datos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace DB_Conexion_1
+{
+    internal class Pokemons_Datos
+    {
+        public List<Pokemons_Desc> listare()
+        {
+            List<Pokemons_Desc> lista = new List<Pokemons_Desc>();
+            SqlConnection conexion = new SqlConnection();
+            SqlCommand comnado = new SqlCommand();
+            SqlDataReader lector;
+
+            try
+            {
+                conexion.ConnectionString = " server = .\\SQLEXPRESS ; database = POKEDEX_DB , integrated security = true";
+                comnado.CommandType = System.Data.CommandType.Text;
+                comnado.CommandText = "Select Numero, Nombre, Descripcion From POKEMONS";
+                comnado.Connection =  conexion;
+
+                conexion.Open();
+                lector= comnado.ExecuteReader();
+
+                while(lector.Read())
+                {
+                    Pokemons_Desc aux = new Pokemons_Desc();
+                    aux.Numero = lector.GetInt32(0);
+                    aux.Nombre = (string)lector["Nombre"];
+                    aux.Descripcion = (string)lector["Descripcion"];
+                    lista.Add(aux);
+                }
+
+
+                return lista;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+
+        }
+    }
+}
---
+++ Pokemons_Desc.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB_Conexion_1
+{
+    internal class Pokemons_Desc
+    {
+        public int Numero { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+    }
+}
